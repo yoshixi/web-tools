6bde9126d6bfc0031a6038cba329cc379501a1a8
yoshiki masubuchi <yoshixi.dev@gmail.com>
2023-07-15 17:37:38 +0900

    install some lint packages and made page for diff-viewrt

--- src/app/diffViewer/page.tsx
+++ src/app/diffViewer/page.tsx
@@ -1,16 +1,17 @@
 import Image from 'next/image';
 
 import { Flex, Box, Grid, Text, Link } from '@kuma-ui/core';
+import NextLink from 'next/link';
 
-export default function Home() {
+export default function Page() {
     return (
         <main>
             <Grid gridTemplateColumns="repeat(2, 1fr)" gap={6} pt={10}>
-                <Link bg="gray" href="/diffViewer">
+                <NextLink href="/diffViewer">
                     <Text color="black" fontSize="16px" textAlign="center" fontWeight="bolder">
                         Diff Viewer
                     </Text>
-                </Link>
+                </NextLink>
                 <Box bg="teal" height="80px" as={'button'}>
                     <Text color="black" fontSize="16px">
                         Hello world
